7e232b5d1e4512e3b634afd483e9a0d26cc31b6e
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-06-27 03:15:22 -0300

    refac: criar propriedade para token

--- news.api/src/infra/ListArticlesNewsApiRepo.js
+++ news.api/src/infra/ListArticlesNewsApiRepo.js
@@ -1,8 +1,11 @@
 const axios = require('axios')
 
 class ListArticlesNewsApiRepo {
+    constructor() {
+        this.token = '4a6fd0a5f0e345288a4e862df6c20833'
+    }
     async getArticles() {
-        const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4a6fd0a5f0e345288a4e862df6c20833')
+        const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=' + this.token)
         return response.data.articles.map(article => {
             return {
                 title: article.title,
