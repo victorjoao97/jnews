f7929ab384b70d700e9f7e7471c587df30e93b2c
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-06-27 14:44:00 -0300

    test: listar artigos por paises

--- news.api/src/infra/ListArticlesNewsApiRepo.js
+++ news.api/src/infra/ListArticlesNewsApiRepo.js
@@ -5,15 +5,34 @@ class ListArticlesNewsApiRepo {
         this.token = '4a6fd0a5f0e345288a4e862df6c20833'
     }
     async getArticles() {
-        const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=' + this.token)
-        return response.data.articles.map(article => {
-            return {
-                title: article.title,
-                description: article.description,
-                author: article.author,
-                content: article.content
-            }
-        })
+        try {
+            const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=' + this.token)
+            return response.data.articles.map(article => {
+                return {
+                    title: article.title,
+                    description: article.description,
+                    author: article.author,
+                    content: article.content
+                }
+            })
+        } catch (error) {
+            throw new Error('Error on get articles')
+        }
+    }
+    async getArticlesFromCountry(country) {
+        try {
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${this.token}`)
+            return response.data.articles.map(article => {
+                return {
+                    title: article.title,
+                    description: article.description,
+                    author: article.author,
+                    content: article.content
+                }
+            })
+        } catch (error) {
+            throw new Error('Error on get articles')
+        }
     }
 }
 
--- news.api/src/infra/ListArticlesNewsApiRepo.spec.js
+++ news.api/src/infra/ListArticlesNewsApiRepo.spec.js
@@ -1,6 +1,7 @@
 const { ListArticlesNewsApiRepo } = require('./ListArticlesNewsApiRepo')
+const axios = require('axios')
 
-describe.skip('List News from API', () => {
+describe('List News from API', () => {
     test('Should return a list with a news', async () => {
         const sut =  new ListArticlesNewsApiRepo()
         const response = await sut.getArticles()
@@ -11,8 +12,21 @@ describe.skip('List News from API', () => {
         expect(response[0]).toHaveProperty('author')
         expect(response[0]).toHaveProperty('content')
     })
+    test('Should return a list with a news from Country', async () => {
+        const sut =  new ListArticlesNewsApiRepo()
+        const response = await sut.getArticlesFromCountry('br')
+        expect(response).not.toBeNull()
+        expect(response.length).toBeGreaterThan(0)
+        expect(response[0]).toHaveProperty('title')
+        expect(response[0]).toHaveProperty('description')
+        expect(response[0]).toHaveProperty('author')
+        expect(response[0]).toHaveProperty('content')
+    })
     test('Should throw error if ListArticlesNewsApiRepo throws', async () => {
         const sut =  new ListArticlesNewsApiRepo()
+
+        jest.spyOn(axios, 'get').mockImplementationOnce(() => Promise.reject(new Error('Internal error')))
+
         const promise = sut.getArticles()
         expect(promise).rejects.toThrow('Error on get articles')
     })
