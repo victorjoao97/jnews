41929788c8174590a3cad89812b2cf9869ac1f2a
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-06-27 12:29:43 -0300

    refac: alterar a factory

--- news.api/src/main/factories/ListArticlesFactory.js
+++ news.api/src/main/factories/ListArticlesFactory.js
@@ -1,10 +1,14 @@
 const { ListArticlesController } = require('../../presentation/ListArticlesController')
 const {ApiListArticles} = require('../../data/ApiListArticles')
 const {ListArticlesNewsApiRepo} = require('../../infra/ListArticlesNewsApiRepo')
+const { ApiListArticlesAndSave } = require('../../data/ApiListArticlesAndSave')
+const { ListArticlesMysqlRepo } = require('../../infra/ListArticlesMysqlRepo')
 
 module.exports = () => {
     const listArticlesRepository = new ListArticlesNewsApiRepo()
+    const listArticlesMysqlRepository = new ListArticlesMysqlRepo(process.env.DATABASE_URL)
+    const apiListArticlesAndSave = new ApiListArticlesAndSave(listArticlesMysqlRepository)
     const apiListArticles = new ApiListArticles(listArticlesRepository)
-    const listArticlesController = new ListArticlesController(apiListArticles)
+    const listArticlesController = new ListArticlesController(apiListArticlesAndSave)
     return listArticlesController
 }
